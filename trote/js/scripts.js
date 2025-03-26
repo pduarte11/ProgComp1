359a8e15c731e697f8dc31d0bdc19aabdcdda779
Pedro <pedro15gduarte@gmail.com>
2025-03-25 22:09:45 -0300

    main

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -1,3 +1,12 @@
 function calcular(){
+//JS as variaveis nao possuem tipos//
     alert("funcionou")
+    let mascote, homenagem
+    mascote = document.getElementById("mascote").value
+    alert(mascote)
+    homenagem = document.getElementById("homenagem").value
+    alert(homenagem)
+    let soma = Number(mascote) + Number(homenagem)
+    alert(soma)
 }
+document.getElementById
