075531fa368f91c2d42d5b99bff61be76479e45c
Pedro Duarte <pedro15gduarte@gmail.com>
2025-04-03 19:37:57 -0300

    0.2

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -1,12 +1,40 @@
 function calcular(){
-//JS as variaveis nao possuem tipos//
-    alert("funcionou")
-    let mascote, homenagem
-    mascote = document.getElementById("mascote").value
-    alert(mascote)
-    homenagem = document.getElementById("homenagem").value
-    alert(homenagem)
-    let soma = Number(mascote) + Number(homenagem)
-    alert(soma)
+    // declaração variável sem tipo
+   let soma = 0
+   // recupera o valor da ação social digitado
+   let acaoSocial = document.getElementById("acaoSocial").value
+   soma = soma + Number(acaoSocial)
+   // recupera o valor da homenagem digitado
+   let homenagem = document.getElementById("homenagem").value
+   soma = soma + Number(homenagem)
+   // recupera a qtde de litros de leite
+   let leite = document.getElementById("leite").value
+   soma = soma + (2 * Number(leite))
+   // recupera a qtde de kits 
+   let kit = Number(document.getElementById("kit").value)
+   // recupera a cor da equipe
+   let equipe = document.getElementById("equipe").value
+    // recupera a qtde de latas de suplemento
+   let suplemento = document.getElementById("suplemento").value
+   let pontosKitSupl = 0 // guarda pontuação de kit + supl
+   if (equipe == "Laranja"){
+       // equipe é laranja
+       if (kit >= 97 && suplemento >= 49) {
+           pontosKitSupl = 5000 + ((kit - 97) * 30) + ((suplemento - 49) * 15)
+       }
+       else if (kit >= 78 && suplemento >= 39){
+           pontosKitSupl = 4000 + ((kit - 78) * 30) + ((suplemento - 39) * 15)
+       }
+       else if (kit >= 49 && suplemento >= 25) {
+           pontosKitSupl = 2500 + ((kit - 49) * 30) + ((suplemento - 25) * 15)
+       }
+       else if (kit >= 19 && suplemento >= 10){
+           pontosKitSupl = 1000 + ((kit - 19) * 30) + ((suplemento - 10) * 15)
+       }
+   }
+  
+   soma = soma + pontosKitSupl
+  
+   // devolve o resultado para o HTML
+   document.getElementById("soma").innerHTML = `R$ ${soma.toFixed(2)}`
 }
-document.getElementById
